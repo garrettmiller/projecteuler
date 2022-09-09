389c29df93c502adb27bf2dca90bfd030e1e7e19
Garrett Miller <garrettmiller@gmail.com>
2022-09-09 11:57:22 -0700

    Added a solution to p015, though didn't come up with it myself.

--- p015.py
+++ p015.py
@@ -2,4 +2,11 @@
 #Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
 #How many such routes are there through a 20×20 grid?
-#TODO - solve this
+
+import math
+gridSize = 20
+
+#Didn't come up with this one on my own, but apparently you can find paths through a grid by 
+#dividing the factorial of 2 * gridSize by the square of the gridSize factorial.
+
+print(int(math.factorial(2*gridSize)/math.factorial(gridSize)**2))
