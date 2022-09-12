1db33a8be06d1cb272f048b5b881a03518eb9d1e
Garrett Miller <garrettmiller@gmail.com>
2022-09-12 10:51:44 -0700

    Solved p023

--- p023.py
+++ p023.py
@@ -8,8 +8,6 @@
 
 #Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
-#TODO - Solve this
-
 #Function to find divisors from Martijn Pieters and Anivarth on Stack Overflow, modified by me for Python3, 
 #and to remove self from divisors.  Previously used in p012.
 def getDivisors(n):
@@ -20,41 +18,33 @@ def getDivisors(n):
     #divisors.extend([n]) #Don't include self in list
     return list(set(divisors))
 
-def isPerfect(n):
-    if (sum(getDivisors(n)) == n):
-        return True
-    else:
-        return False
-
 def isAbundant(n):
     if (sum(getDivisors(n)) > n):
         return True
     else:
         return False
 
-def isDeficient(n):
-    if (sum(getDivisors(n)) < n):
-        return True
-    else:
-        return False
-
 abundantList = []
+abundantSumList = []
 resultList = []
 
-#All integers greater than 28123 can be written as sum of two abundant numbers 
+#All integers greater than 28123 can be written as sum of two abundant numbers, so we only need to test that far. 
+#Let's create a list of abundant numbers, why not
+for i in range(11,28124):
+    if isAbundant(i):
+        abundantList.append(i)
+
+#Then let's create a list of possible sums of abundant numbers
+for n in abundantList:
+    for y in abundantList:
+        abundantSumList.append(n+y)
+#and make it unique
+abundantSumList = set(abundantSumList)
+
+#Then let's see if our numbers are in it
 for i in range(1,28124):
-    #Let's create a list of abundant numbers, why not
-        if isAbundant(i):
-            abundantList.append(i)
-
-for i in range(1,len(abundantList)):
-    matchFlag = False
-    for y in range(1,len(abundantList)):
-        if abundantList[i] + abundantList[y] == i:
-            matchFlag = True
-            break
-    if not matchFlag:
+    if i not in abundantSumList:
         resultList.append(i)
+        print(f"Appending Result: {i}, sum is {(sum(resultList))}")
 
-print(abundantList)
 print(sum(resultList))
