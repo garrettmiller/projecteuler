e394990a3427343d7f5bd8558d0b0bf1108730ae
Garrett Miller <garrettmiller@gmail.com>
2022-09-12 11:09:46 -0700

    Removed todo from p024 after solving

--- p024.py
+++ p024.py
@@ -6,8 +6,6 @@
 #012   021   102   120   201   210
 #What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-#TODO - Solve this
-
 import itertools
 
 digitList = [0,1,2,3,4,5,6,7,8,9]
