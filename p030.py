88e0bfb41e03d7aa27a7a0a3a7988cbd8a9b8c80
Garrett Miller <garrettmiller@gmail.com>
2022-09-13 09:37:37 -0700

    Solved p030

--- p030.py
+++ p030.py
@@ -9,3 +9,16 @@
 #The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 
 #Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+fifthPowerList = []
+
+#Let's pick a nice big number to test up to, there's a mathier way to figure out this maximum I'm sure.
+for i in range(2,1000000):
+    loopSum = 0 
+    #Go through the digits iteratively
+    for x in str(i):
+        loopSum = loopSum + int(x)**5
+    if loopSum == i:
+        fifthPowerList.append(i)
+
+print(f"Numbers that can be expressed as fifth powers of digits are {fifthPowerList} and the sum is {sum(fifthPowerList)}. ")
