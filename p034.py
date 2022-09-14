b2090db4df1cc941b62caa724352adde8ef43ceb
Garrett Miller <garrettmiller@gmail.com>
2022-09-14 14:32:35 -0700

    Solved p034

--- p034.py
+++ p034.py
@@ -5,4 +5,18 @@
 
 #Note: As 1! = 1 and 2! = 2 are not sums they are not included.
 
-#TODO - Solve this 
+factorialList = []
+
+import math
+
+#Pick an arbitrarily high number to test to
+for i in range(3,1000000):
+    localSum = 0
+    for digit in str(i):
+        localSum = localSum + math.factorial(int(digit))
+    if localSum == i:
+        factorialList.append(i)
+
+print("All numbers which are equal to sum of the factorials of their digits are: ")
+print(factorialList)
+print(f"and the sum is: {sum(factorialList)}")
