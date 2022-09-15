785c99d012a681db507aaa069d467132d2e332f3
Garrett Miller <garrettmiller@gmail.com>
2022-09-14 20:17:47 -0700

    Added some comments to check my implementation, p035 not working yet

--- p035.py
+++ p035.py
@@ -7,7 +7,27 @@
 #How many circular primes are there below one million?
 
 #NOTE - super slow, I'm sure there's smarter ways to speed this up
-#TODO - finish testing
+#TODO - fix
+
+#Prime family: [2]
+#Prime family: [3]
+#Prime family: [5]
+#Prime family: [7]
+#Prime family: [11]
+#Prime family: [13, 31]
+#Prime family: [17, 71]
+#Prime family: [37, 73]
+#Prime family: [79, 97]
+#Prime family: [113, 131, 311]
+#Prime family: [197, 971, 719]
+#Prime family: [199, 991, 919]
+#Prime family: [337, 373, 733]
+#Prime family: [1193, 1931, 9311, 3119]
+#Prime family: [3779, 7793, 7937, 9377]
+#Prime family: [11939, 19391, 93911, 39119, 91193]
+#Prime family: [19937, 99371, 93719, 37199, 71993]
+#Prime family: [193939, 939391, 393919, 939193, 391939, 919393]
+#Prime family: [199933, 999331, 993319, 933199, 331999, 319993]
 
 import itertools
 import math #import math for speed
@@ -19,14 +39,16 @@ def is_prime(n):
             return False
     return True
 
-circularPrimeCount = 0
+limit=1000
+circularPrimeCount = 1 #We're skipping 2 so start this at 1
 listOfRotations = []
 compositeFlag = False
 
-for i in range(3,1000000,2): #no need to check evens
-    print(f"Testing permutations of {i}, and have found {circularPrimeCount+1} circular primes so far...")
+for i in range(3,limit,2): #no need to check evens
+    print(f"Testing permutations of {i}, and have found {circularPrimeCount} circular primes so far...")
     digitList = []
     listOfRotations = []
+    compositeFlag = False
     #Build a list that we can permute
     for digit in str(i):
         digitList.append(digit)
@@ -41,7 +63,6 @@ for i in range(3,1000000,2): #no need to check evens
         #and throw it into a current list of rotations
         listOfRotations.append(thisNumber)
         #Check each one in that list to determine if each are prime, initializing a flag value
-        compositeFlag = False
         for candidatePrime in listOfRotations:
             if is_prime(candidatePrime) == False:
                 compositeFlag = True
@@ -51,5 +72,4 @@ for i in range(3,1000000,2): #no need to check evens
     if compositeFlag == False:
         circularPrimeCount = circularPrimeCount + 1
     
-#Add 1 to result since we're starting from 3
-print(f"The number of circular primes below one million is: {circularPrimeCount+1}")
+print(f"The number of circular primes below {limit} is: {circularPrimeCount}")
