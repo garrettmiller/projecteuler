8111f076879e576727a50edc813a22856e26232c
Garrett Miller <garrettmiller@gmail.com>
2022-09-19 10:55:34 -0700

    Solved p040, need to clean up probably

--- p040.py
+++ p040.py
@@ -9,3 +9,10 @@
 #If dn represents the nth digit of the fractional part, find the value of the following expression.
 
 #d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
+longStr = "0."
+for i in range(1,1000001):
+    longStr = longStr + str(i)
+
+result = int(longStr[(1+1)]) * int(longStr[(10+1)]) * int(longStr[(100+1)]) * int(longStr[(1000+1)]) * int(longStr[(10000+1)]) * int(longStr[(100000+1)]) * int(longStr[(1000000+1)])
+print(result)
