23f1c9481bd33084b36342086988e53b0b7bf80f
Garrett Miller <garrettmiller@gmail.com>
2022-09-20 11:44:44 -0700

    Partial incorrect solution for p037

--- p037.py
+++ p037.py
@@ -7,4 +7,30 @@
 
 #NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
-#TODO - solve this
+#TODO - fix this
+
+def is_prime(n):
+    upper = int(n**.5)+1
+    for i in range(2,upper):
+        if (n % i) == 0:
+            return False
+    return True
+
+truncatablePrimeList = []
+
+for i in range(11,1000000):
+    compositeFlag = False
+    numString = str(i)
+    if is_prime(i):
+        for x in range((len(numString)-1),1,-1):
+            print(f"Testing {numString}, and substrings {numString[:x]} and {numString[(x*-1):]}")
+            if (is_prime(int(numString[:x])) == False) or (is_prime(int(numString[(x*-1):])) == False):
+                compositeFlag = True
+                break
+            else:
+                continue
+        if compositeFlag == False:
+            print(f"TRUNCATABLE PRIME FOUND: {numString}")
+            truncatablePrimeList.append(i)
+        
+print(f"The sum of truncatable primes is: {sum(truncatablePrimeList)}")
