a11311defac1d91b8e4aca88bb8f65107892fcef
Garrett Miller <garrettmiller@gmail.com>
2022-09-20 13:43:04 -0700

    Found problem in p037, still not giving right result

--- p037.py
+++ p037.py
@@ -7,7 +7,7 @@
 
 #NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
-#TODO - fix this
+#TODO - fix this, for some reason "19" is showing up and its truncation check should disqualify it by having "9" in the last digit
 
 def is_prime(n):
     upper = int(n**.5)+1
@@ -18,13 +18,14 @@ def is_prime(n):
 
 truncatablePrimeList = []
 
-for i in range(11,1000000):
+for i in range(11,100,2):
     compositeFlag = False
     numString = str(i)
     if is_prime(i):
         for x in range((len(numString)-1),1,-1):
             print(f"Testing {numString}, and substrings {numString[:x]} and {numString[(x*-1):]}")
             if (is_prime(int(numString[:x])) == False) or (is_prime(int(numString[(x*-1):])) == False):
+                print(f"Composite detected, {int(numString[:x])} or {int(numString[(x*-1)])}")
                 compositeFlag = True
                 break
             else:
@@ -32,5 +33,5 @@ for i in range(11,1000000):
         if compositeFlag == False:
             print(f"TRUNCATABLE PRIME FOUND: {numString}")
             truncatablePrimeList.append(i)
-        
-print(f"The sum of truncatable primes is: {sum(truncatablePrimeList)}")
+            
+print(f"The list of truncatable primes is {truncatablePrimeList} and sum of truncatable primes is: {sum(truncatablePrimeList)}")
