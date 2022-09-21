ceb185d37f266c84af39e3998f9831988b60ea22
Garrett Miller <garrettmiller@gmail.com>
2022-09-21 11:30:26 -0700

    Solved p036, ez

--- p036.py
+++ p036.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 #The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
 
-#ind the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+#Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 
 #(Please note that the palindromic number, in either base, may not include leading zeros.)
 
 #TODO - solve this
+
+dromeList = []
+
+for i in range(1, 1000000):
+    stringDrome = str(i)
+    if stringDrome == stringDrome[::-1]: #neat little trick to reverse a string
+        #Now let's see if it's a palindrome in base 2
+        possibleStringBinaryDrome = str(bin(i))
+        if possibleStringBinaryDrome[2::] == possibleStringBinaryDrome[:1:-1]:
+            print(f"Decimal/Binary Palindrome Found: {stringDrome}, {possibleStringBinaryDrome[2::]}")
+            dromeList.append(i)
+    
+print(str(sum((dromeList))))
