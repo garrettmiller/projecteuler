fe636265aa2355b4729bc55f48cae6ce12a09b74
Garrett Miller <garrettmiller@gmail.com>
2022-09-21 11:44:19 -0700

    Solved p038

--- p038.py
+++ p038.py
@@ -10,4 +10,19 @@
 
 #What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
 
-#TODO - solve this
+largestNum = 123456789 #Start with the lowest possible pandigital
+
+for i in range(1,10000):
+    productString = ""
+    for x in range(1,10):
+        product = i*x
+        productString = productString + str(product)
+        #Only continue testing if we're exactly 9 digits long (1-9)
+        if len(str(productString)) == 9:
+            #See if we're all unique numbers (1-9) by comparing length against set() length, and make sure there's no 0 since we're doing 1-9 only. 
+            if (len(set(productString)) == len(productString)) and '0' not in productString:
+                if int(productString) > largestNum:
+                    largestNum = int(productString)
+                    baseNum = i
+     
+print(f"The largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) is: {largestNum}, formed from {i}")
