fe0b1ec97801f2095167e2b987e5ab9a0406fcc6
Garrett Miller <garrettmiller@gmail.com>
2022-09-21 15:46:14 -0700

    Solved p039

--- p039.py
+++ p039.py
@@ -5,4 +5,20 @@
 
 #For which value of p ≤ 1000, is the number of solutions maximised?
 
-#TODO - solve this
+from collections import Counter
+
+perimeterList = [] 
+
+for a in range(1,500):
+    for b in range(1,500):
+        #Hooray triangles
+        c = (a**2 + b**2)**.5
+        if int(c) == c:
+            p = a+b+c
+            if p <= 1000:
+                print(f"Valid right triangle found with sides: {a},{b},{c}, with p={p}")
+                #Append it to a list, then we'll just count how many times each perimeter appears
+                perimeterList.append(int(p))
+
+counter = Counter(perimeterList)
+print(f"The perimeter <= 1000 which yield the greatest number of right triangle solutions is (perimeter, solutions): {counter.most_common(1)})")
