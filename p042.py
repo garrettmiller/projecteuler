b87355dcfb0c32f9f73a10633d74386141ad9af0
Garrett Miller <garrettmiller@gmail.com>
2022-09-22 09:33:11 -0700

    Solved p042

--- p042.py
+++ p042.py
@@ -10,3 +10,34 @@
 #Using a 16K text file containing nearly two-thousand common English words, how many are triangle words?
 
 #TODO - Solve this
+
+import csv
+import string 
+
+wordList = []
+triangleList = []
+bigSum = 0
+triangleWords = 0
+
+#Build a list of words
+with open("p042_words.txt", 'r', newline='\n') as file:
+    csvreader = csv.reader(file, quoting=csv.QUOTE_ALL, delimiter=',')
+    for row in csvreader:
+        for name in row:
+            wordList.append(name)
+
+#Build a list of triangle numbers up to 1000
+for n in range(1,1000):
+    triangleList.append(0.5*(n*(n+1)))
+
+#Then iterate through the wordlist to see if the value is in 
+
+for word in wordList:
+    wordSum = 0
+    for character in word:
+        wordSum = wordSum + (string.ascii_uppercase.index(character)+1)
+    if int(wordSum) in triangleList:
+        print(f"Triangle word detected: {word}, {wordSum}")
+        triangleWords = triangleWords + 1
+    
+print(f"The number of triangle words in the list is: {triangleWords}")
