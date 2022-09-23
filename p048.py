210d6f2054013af70f879cd00ac6db8b642376da
Garrett Miller <garrettmiller@gmail.com>
2022-09-23 12:10:56 -0700

    Solved p048

--- p048.py
+++ p048.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-#The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+#The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
-#Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+#Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 #TODO - Solve this
+
+termList = []
+
+for i in range(1,1001):
+    termList.append(int(i**i))
+
+print(str(sum(termList))[-10:])
