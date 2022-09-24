70d33294060b7ce779886a0bd568a1cc75e639f3
Garrett Miller <garrettmiller@gmail.com>
2022-09-23 18:41:45 -0700

    Solved p046

--- p046.py
+++ p046.py
@@ -12,4 +12,37 @@
 
 #What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 
-#TODO - Solve this
+def is_prime(n):
+    upper = int(n**.5)+1
+    for i in range(2,upper):
+        if (n % i) == 0:
+            return False
+    return True
+
+#Start with 2, 3 so we don't have to find them
+primeList = [2,3]
+
+#Build a list of primes
+for i in range(5,10000,2):
+    if is_prime(i):
+        primeList.append(i)
+
+#Then just test every odd number starting at first odd composite?
+for odd in range(9,10000,2):
+    solutionFlag = False
+    #Make sure it's composite
+    if is_prime(odd) == False:
+        for prime in primeList:
+            #Make sure we're still less than the tested odd for efficiency
+            if prime < odd:
+                for square in range(1,50):
+                    if (prime + (2 * (square**2))) == odd:
+                        solutionFlag = True
+                        #print(f"Solution found for {prime} + 2*{square}^2 = {odd}, so solutionFlag = {solutionFlag}")
+                        break
+    else: #If we're prime, disregard
+        solutionFlag = True
+
+    if solutionFlag == False:
+        print(f"The smallest odd composite that cannot be written as the sum of a prime and 2x a square is: {odd}")
+        quit()
