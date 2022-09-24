80cc02ea13cb1e719e38989bb7007fa636a2ba1d
Garrett Miller <garrettmiller@gmail.com>
2022-09-23 18:21:23 -0700

    Incomplete progress on p047

--- p047.py
+++ p047.py
@@ -12,4 +12,24 @@
 
 #Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
 
-#TODO - Solve this
+#TODO - Fix this, incomplete
+
+#Prime factorization algorithm from Will Ness on Stack Overflow
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if (n % i) != 0:
+            i = i + 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+for i in range(1,100000):
+    factorList = prime_factors(i)
+    if len(set(factorList)) == 4:
+        print(f"The first four consecutive integers to have four prime factors are: {str(i)}")
+        quit()
