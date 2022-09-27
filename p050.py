89b9fa71ab8114bc7f7f169721627472843e5020
Garrett Miller <garrettmiller@gmail.com>
2022-09-26 18:53:50 -0700

    Preliminary, incorrect solution for p050

--- p050.py
+++ p050.py
@@ -9,4 +9,30 @@
 
 #Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
-#TODO - Solve this
+#TODO - Fix this, it's currently not accounting for situations where we don't begin at 2
+
+def is_prime(n):
+    upper = int(n**.5)+1
+    for i in range(2,upper):
+        if (n % i) == 0:
+            return False
+    return True
+
+#Start with 2, 3 so we don't have to find them
+primeList = [2,3]
+
+#Build a list of primes
+for i in range(5,1000000,2):
+    if is_prime(i):
+        primeList.append(i)
+
+for prime in primeList:
+    total = 0
+    primeCount = 0
+    for otherPrime in primeList:
+        total = total + otherPrime
+        primeCount = primeCount + 1
+        if total == prime:
+            print(f"Prime found as sum of consecutive primes: {str(prime)}, with number of primes {str(primeCount)}")
+        elif total > prime:
+            break
