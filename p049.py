c01cd15586a785858611b743899cd6ef9cd2ce02
Garrett Miller <garrettmiller@gmail.com>
2022-09-26 20:08:27 -0700

    Progress on p049 but needs fixed

--- p049.py
+++ p049.py
@@ -6,7 +6,7 @@
 
 #What 12-digit number do you form by concatenating the three terms in this sequence?
 
-#TODO - solve this
+#TODO - fix this
 
 from itertools import permutations
 
@@ -17,7 +17,33 @@ def is_prime(n):
             return False
     return True
 
-for i in range(1000,10000):
-    termList = []
-    permutationList = list(permutations(stringNums))
-    i = 
+for i in range(1,10000):
+    #This feels messy but seems to be the best way I know to get all permutations of an int
+    listOfPermutations = []
+    permutationList = list(permutations(str(i)))
+    #This will track if we found a valid solution or not
+    flag = True
+    #Then for each set of numbers in that permutationList,  
+    for number in permutationList:
+        #reassemble it into an int
+        thisNumber = int("".join(number))
+        #and throw it into a current list of permutations
+        listOfPermutations.append(thisNumber)
+    for n in range(1,5000):
+        #Break if we get too high for efficiency
+        if (i+2*n) > 9999:
+            break
+        termList = []
+        termList.append(i)
+        termList.append(i+n)
+        termList.append(i+(2*n))
+        #print(f"TermList is {termList}, and permutations are {listOfPermutations}")
+        for term in termList:
+            if term not in listOfPermutations or is_prime(term) == False:
+                flag = False
+                break
+            else:
+                print(f"Continuing with i {i} and n {n} and termList {termList}...")
+        if flag == True:
+            print(f"Sequence found: {termList}")
+            quit()
