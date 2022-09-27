c3201e73d42483ac692595d564e4915c3d0ca490
Garrett Miller <garrettmiller@gmail.com>
2022-09-26 20:25:53 -0700

    Testing another solution with primes for efficiency, still not working

--- p049.py
+++ p049.py
@@ -17,38 +17,49 @@ def is_prime(n):
             return False
     return True
 
-for i in range(1000,10000):
-    #This feels messy but seems to be the best way I know to get all permutations of an int
-    listOfPermutations = []
-    permutationList = list(permutations(str(i)))
+primeList = []
+
+#Build a list of primes
+for i in range(1001,10000,2):
+    if is_prime(i):
+        primeList.append(i)
+
+#Let's just test primes for speed
+for prime in primeList:
     #This will track if we found a valid solution or not
     flag = True
+    
+    #This feels messy but seems to be the best way I know to get all permutations of an int
+    listOfPermutations = []
+    permutationList = set(permutations(str(prime)))
     #Then for each set of numbers in that permutationList,  
     for number in permutationList:
         #reassemble it into an int
         thisNumber = int("".join(number))
-        #and throw it into a current list of permutations
-        listOfPermutations.append(thisNumber)
+        #and throw it into a current list of permutations if it's 4 digits
+        if len(str(thisNumber)) == 4:
+            listOfPermutations.append(thisNumber)
+    #Then let's increment by an increasing amount             
     for n in range(1,5000):
         #Break if we get too high for efficiency
-        if (i+2*n) > 9999:
+        if (prime+(2*n)) > 9999:
             break
         termList = []
-        termList.append(i)
-        termList.append(i+n)
-        termList.append(i+(2*n))
-        print(f"TermList is {termList}, and permutations are {listOfPermutations}")
+        termList.append(prime)
+        termList.append(prime+n)
+        termList.append(prime+(2*n))
+        #print(f"TermList is {termList}, and permutations are {listOfPermutations}")
         for term in termList:
             if term not in listOfPermutations:
-                print(f"{term} is not in {listOfPermutations}!")
+                #print(f"{term} is not in {listOfPermutations}!")
                 flag = False
                 break 
             if is_prime(term) == False:
-                print(f"{term} is not prime!")
+                #print(f"{term} is not prime!")
                 flag = False
                 break
-            else:
-                print(f"Continuing with i {i} and n {n} and termList {termList}...")
+            #else:
+                #print(f"Continuing with prime {prime} and n {n} and termList {termList}...")
         if flag == True:
             print(f"Sequence found: {termList}")
             quit()
