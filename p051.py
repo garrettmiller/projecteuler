c7bd068cf336b518d615b86e01213dbfaa3250b0
Garrett Miller <garrettmiller@gmail.com>
2022-11-10 13:42:32 -0800

    Added todo to p051 since it's not returning expected result on Python 3.11?

--- p051.py
+++ p051.py
@@ -8,6 +8,8 @@
 
 #Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family.
 
+#TODO - fix this
+
 def is_prime(n):
     upper = int(n**.5)+1
     for i in range(2,upper):
